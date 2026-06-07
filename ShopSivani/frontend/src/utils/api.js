5936dade3d3e3549073a37f5e2d3bfee3662ed12
Bonisha005 <sivanicsdmvgr00@gmail.com>
2026-06-07 16:42:22 +0530

    Fix API URL for production

--- ShopSivani/frontend/src/utils/api.js
+++ ShopSivani/frontend/src/utils/api.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
-const api = axios.create({ baseURL: '/api' });
+const api = axios.create({
+  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api'
+});
 
-// Attach JWT token to every request
 api.interceptors.request.use(config => {
   const user = JSON.parse(localStorage.getItem('shopsivani_user'));
   if (user?.token) config.headers.Authorization = `Bearer ${user.token}`;
